0afa32bf2442fdcdcb41a650130d629e6aa05080
ACACIANha <gkehdrn95@netand.co.kr>
2023-04-05 10:22:42 +0900

    first commit

---
+++ src/main/java/com/example/springbootmonitoring/SpringbootMonitoringApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMonitoringApplication {
+
+	//https://covenant.tistory.com/244 실습
+	public static void main( String[] args ) {
+		SpringApplication.run( SpringbootMonitoringApplication.class, args );
+	}
+
+}
